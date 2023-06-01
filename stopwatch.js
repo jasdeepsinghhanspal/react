86ff6ee9eabc16409e498c03632fb10086b4c7e5
jasdeepsinghhanspal <86922612+jasdeepsinghhanspal@users.noreply.github.com>
2023-06-01 20:05:14 +0530

    stopwatch.js
    stopwatch program using useState,useEffect

---
+++ stopwatch.js
@@ -0,0 +1,47 @@
+//stopwatch.js
+import React, { useState, useEffect } from 'react';
+
+function Stopwatch() {
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let intervalId;
+
+    if (isRunning) {
+      intervalId = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [isRunning]);
+
+  const handleStart = () => {
+    setIsRunning(true);
+  };
+
+  const handleStop = () => {
+    setIsRunning(false);
+  };
+
+  const handleReset = () => {
+    setTime(0);
+    setIsRunning(false);
+  };
+
+  return (
+    <div>
+      <h1>Stopwatch</h1>
+      <p>Time: {time} seconds</p>
+
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
+      <button onClick={handleReset}>Reset</button>
+    </div>
+  );
+}
+
+export default Stopwatch;
