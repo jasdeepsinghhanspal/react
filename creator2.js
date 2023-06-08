882c6fac9c90355b25fa5b8d2760ddfab2c719f1
jasdeep singh hanspal <86922612+jasdeepsinghhanspal@users.noreply.github.com>
2023-06-08 20:16:15 +0530

    creator2.js

---
+++ creator2.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { MyComponent, MyClassComponent } from './creator1';
+
+const App = () => {
+  return (
+    <div>
+      <MyComponent />
+      <MyClassComponent />
+    </div>
+  );
+};
+
+export default App;
